55b5528344c02f74b904ddd30688e5095230832d
Riccardo Finotello <riccardo.finotello@gmail.com>
2020-07-22 10:16:52 +0200

    Add ablation study
    Signed-off-by: Riccardo Finotello <riccardo.finotello@gmail.com>

---
+++ ablation/ablation-study.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+sns.set()
+
+data = [('inception_1d_noout', '30%', 0.97),
+        ('inception_1d_out', '30%', 0.89),
+        ('inception_2d_noout', '30%', 0.69),
+        ('inception_2d_out', '30%', 0.62),
+        ('conv_noout', '30%', 0.79),
+        ('conv_out', '30%', 0.68),
+        ('inception_1d_noout', '80%', 0.99),
+        ('inception_2d_noout', '80%', 0.86),
+        ('inception_1d_out', '80%', 0.99),
+        ('inception_2d_out', '80%', 0.78),
+        ('conv_noout', '80%', 0.93),
+        ('conv_out', '80%', 0.85)
+        ]
+data = pd.DataFrame(data, columns=('id', 'percentage', 'accuracy'))
+
+labels = ['Inception, 1d, no outliers',
+          'Inception, 1d, outliers',
+          'Inception, 2d, no outliers',
+          'Inception, 2d, outliers',
+          'convnet, no outliers',
+          'convnet, outliers'
+         ]
+
+_, ax = plt.subplots(1, 1, figsize=(7, 5))
+
+plot = sns.barplot(x='id', hue='percentage', y='accuracy', data=data,
+                   orient='v',
+                   ax=ax
+                  )
+ax.set_xlabel("")
+ax.set_yticks(np.arange(0.0, 1.0 + 0.1, 0.1))
+ax.set_ylim([0.0, 1.0])
+ax.set_xticklabels(labels,
+                   rotation=45,
+                   va='top',
+                   ha='right'
+                  )
+ax.legend(bbox_to_anchor=(0.0, 0.0),
+          loc="lower left",
+          title="training data",
+          ncol=1
+         )
+for p in plot.patches:
+    plot.annotate('{:d}%'.format(int(p.get_height() * 100)),
+                  xy=(p.get_x() + p.get_width() / 2,
+                      p.get_height()
+                     ),
+                  ha='center',
+                  va='bottom', fontsize=10,
+                 )
+
+plt.tight_layout()
+plt.savefig('ablation.pdf', dpi=150, format='pdf')
---
+++ ablation/summary-results-pre-cnn.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+sns.set()
+
+data = {'baseline': 0.45,
+        'svm_30': 0.55,
+        'svm_80': 0.68,
+        'he_reg': 0.37,
+        'bull_reg': 0.75,
+        'bull_class': 0.85,
+        'inception_30': 0.97,
+        'inception_80': 0.99,
+       }
+data = pd.DataFrame(data, index=[0])
+
+labels = ['lin. reg. (80%)',
+          'SVM RBF (reg., 30%)',
+          'SVM RBF (reg., 80%)',
+          'He (reg., 63%)',
+          'Bull et al. (reg., 70%)',
+          'Bull et al. (class., 70%)',
+          'Inception (30%)',
+          'Inception (80%)'
+         ]
+
+_, ax = plt.subplots(1, 1, figsize=(6,5))
+
+plot = sns.barplot(data=data,
+                   color=sns.color_palette('muted',1)[0],
+                   orient='h',
+                   ax=ax
+                  )
+ax.set_xlim([0.0, 1.0])
+ax.set_yticklabels(labels)
+for p in plot.patches:
+    plot.annotate('{:d}%'.format(int(p.get_width() * 100)),
+                  xy=(p.get_width() + 0.01,
+                      p.get_y() + p.get_height() / 2
+                     ),
+                  ha='left',
+                  va='center'
+                 )
+
+plt.tight_layout()
+plt.savefig('summary_hor.pdf', dpi=150, format='pdf')
+
+
+_, ax = plt.subplots(1, 1, figsize=(6,5))
+
+plot = sns.barplot(data=data,
+                   color=sns.color_palette('muted',1)[0],
+                   orient='v',
+                   ax=ax
+                  )
+ax.set_ylim([0.0, 1.0])
+ax.set_yticks(np.arange(0.1, 1.0 + 0.1, 0.1))
+ax.set_xticklabels(labels,
+                   rotation=30,
+                   va='top',
+                   ha='right'
+                  )
+for p in plot.patches:
+    plot.annotate('{:d}%'.format(int(p.get_height() * 100)),
+                  xy=(p.get_x() + p.get_width() / 2,
+                      p.get_height()
+                     ),
+                  ha='center',
+                  va='bottom'
+                 )
+
+plt.tight_layout()
+plt.savefig('summary_ver.pdf', dpi=150, format='pdf')
